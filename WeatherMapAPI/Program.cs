64ccea9565c94e1584955f22ce77ef451d1d810f
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-10-28 16:08:28 -0700

    completed weather API exercise

---
+++ WeatherMapAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
+{
+    internal class OpenWeatherMapAPI
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            var zipCode = Console.ReadLine();
+            
+            var apiCall =$"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees Fahrenheit in your location.");
+        }
+    }
+}
---
+++ WeatherMapAPI/WeatherMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON;
+
+public class WeatherMap
+{
+    public static double GetTemp(string apiCall)
+    {
+        var client = new HttpClient();
+        
+        var response = client.GetStringAsync(apiCall).Result;
+
+        var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+        
+        return temp;
+    }
+}
